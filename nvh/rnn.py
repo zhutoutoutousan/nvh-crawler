eaa93f7bcd979d7e1c2c9801f3613d1f094cc6e1
zhutoutoutousan <31960995+zhutoutoutousan@users.noreply.github.com>
2023-10-10 23:31:12 +0800

    dang dis is hard

--- nvh/rnn.py
+++ nvh/rnn.py
@@ -38,3 +38,79 @@ input_tensor = letter_to_tensor('A')
 hidden_tensor = rnn.init_hidden()
 
 output, next_hidden = rnn(input_tensor, hidden_tensor)
+print(output.size())
+print(next_hidden.size())
+
+# whole sequence/name
+input_tensor = line_to_tensor('Albert')
+hidden_tensor = rnn.init_hidden()
+
+output, next_hidden = rnn(input_tensor[0], hidden_tensor)
+print(output.size())
+print(next_hidden.size())
+
+def category_from_output(output):
+    category_idx = torch.argmax(output).item()
+    return all_categories[category_idx]
+
+print(category_from_output(output))
+
+criterion = nn.NLLLoss()
+learning_rate = 0.005
+optimizer = torch.optim.SGD(rnn.parameters(), lr=learning_rate)
+
+def train(category_tensor, line_tensor):
+    hidden = rnn.init_hidden()
+    rnn.zero_grad()
+    for i in range(line_tensor.size()[0]):
+        output, hidden = rnn(line_tensor[i], hidden)
+    loss = criterion(output, category_tensor)
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+    return output, loss.item()
+
+current_loss = 0
+all_losses = []
+plot_steps, print_steps = 1000, 5000
+n_iters = 100000
+
+for i in range(n_iters):
+    category, line, category_tensor, line_tensor = random_training_example(category_lines, all_categories)
+    output, loss = train(category_tensor, line_tensor)
+    current_loss += loss
+
+    if (i+1) % plot_steps == 0:
+        all_losses.append(current_loss / plot_steps)
+        current_loss = 0
+
+    if (i+1) % print_steps == 0:
+        guess = category_from_output(output)
+        correct = 'CORRECT' if guess == category else f'WRONG ({category})'
+        print(f'{i+1} {(i+1)/n_iters*100} {loss:.4f} {line} / {guess} {correct}')
+
+plt.figure()
+plt.plot(all_losses)
+plt.show()
+
+def predict(input_line, n_predictions=3):
+    print(f'\n> {input_line}')
+    with torch.no_grad():
+        line_tensor = line_to_tensor(input_line)
+        hidden = rnn.init_hidden()
+        for i in range(line_tensor.size()[0]):
+            output, hidden = rnn(line_tensor[i], hidden)
+        
+        topv, topi = output.topk(n_predictions, 1, True)
+        predictions = []
+        for i in range(n_predictions):
+            value = topv[0][i].item()
+            category_idx = topi[0][i].item()
+            print(f'{value:.2f} {all_categories[category_idx]}')
+            predictions.append([value, all_categories[category_idx]])
+
+while(true):
+    sentence = input('Input sentence:')
+    if sentence == 'quit':
+        break
+    predict(sentence)
